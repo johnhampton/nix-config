8ad30a317a8a3a645e37bae678e2983070ac62a6
John Hampton <john.hampton@stanfordalumni.org>
2022-11-14 15:17:20 -0800

    Install darwin-nix

---
+++ .config/nixpkgs/darwin.nix
@@ -0,0 +1,44 @@
+{ inputs, config, pkgs, lib, ... }:
+{
+  nix.settings = {
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://hydra.iohk.io"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+
+    trusted-users = [
+      "john"
+      "@admin"
+    ];
+    auto-optimise-store = true;
+
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+
+    keep-outputs = true;
+    keep-derivations = true;
+
+    extra-platforms = lib.mkIf (pkgs.system == "aarch64-darwin") [ "x86_64-darwin" "aarch64-darwin" ];
+  };
+
+  nix.configureBuildUsers = true;
+
+  nix.package = pkgs.nixUnstable;
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+
+
+  system.stateVersion = 4;
+
+
+
+  programs.fish.enable = true;
+  programs.zsh.enable = true;
+}
--- .config/nixpkgs/flake.nix
+++ .config/nixpkgs/flake.nix
@@ -1,39 +1,128 @@
 {
-  description = "Home Manager configuration of John Hampton";
+  description = "Nix configuration of John Hampton";
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    plugin-foreign-env = {
-      url = "github:oh-my-fish/plugin-foreign-env";
-      flake = false;
-    };
+    # Package sets
+    nixpkgs-unstable.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    nixpkgs-master.url = "github:nixos/nixpkgs/master";
+    nixpkgs-stable.url = github:NixOS/nixpkgs/nixpkgs-22.05-darwin;
+    nixos-stable.url = github:NixOS/nixpkgs/nixos-22.05;
 
+    # Environment/system management
+    darwin.url = github:LnL7/nix-darwin;
+    darwin.inputs.nixpkgs.follows = "nixpkgs-unstable";
+    home-manager.url = github:nix-community/home-manager;
+    home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
+
+    flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
+    flake-utils.url = "github:numtide/flake-utils";
+
+    plugin-foreign-env = { url = "github:oh-my-fish/plugin-foreign-env"; flake = false; };
   };
 
-  outputs = { nixpkgs, home-manager, plugin-foreign-env, ... }:
+  outputs = { self, nixpkgs, darwin, home-manager, flake-utils, plugin-foreign-env, ... }@inputs:
     let
-      system = "x86_64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
+      inherit (darwin.lib) darwinSystem;
+      inherit (inputs.nixpkgs-unstable.lib) attrValues makeOverridable optionalAttrs singleton;
+
+      nixpkgsConfig = {
+        config = { allowUnfree = true; };
+        overlays = attrValues self.overlays;
+      };
+
+
+      homeManagerStateVersion = "22.05";
+      homeManagerCommonConfig = {
+        imports = [
+          ./home.nix
+          { home.stateVersion = homeManagerStateVersion; }
+        ];
+      };
+
+      nixDarwinCommonModules = [
+        ./darwin.nix
+        home-manager.darwinModules.home-manager
+        (
+          { config, lib, pkgs, ... }:
+          let
+            primaryUser = "john";
+          in
+          {
+            nixpkgs = nixpkgsConfig;
+            # Hack to support legacy worklows that use `<nixpkgs>` etc.
+            # nix.nixPath = { nixpkgs = "$HOME/.config/nixpkgs/nixpkgs.nix"; };
+            # `home-manager` config
+            users.users.${primaryUser}.home = "/Users/${primaryUser}";
+            home-manager.useGlobalPkgs = true;
+            home-manager.users.${primaryUser} = homeManagerCommonConfig;
+            home-manager.extraSpecialArgs = {
+              inherit plugin-foreign-env;
+            };
+          }
+        )
+      ];
     in
     {
-      homeConfigurations.john = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-        extraSpecialArgs = {
-          inherit plugin-foreign-env;
+      darwinConfigurations = {
+        Ava = darwinSystem {
+          system = "x86_64-darwin";
+          modules = nixDarwinCommonModules ++ [
+            {
+              networking = {
+                # computerName = "Ava";
+                knownNetworkServices = [
+                  "USB 10/100/1000 LAN"
+                  "Wi-Fi"
+                  "iPhone USB"
+                  "Thunderbolt Bridge"
+                ];
+              };
+            }
+          ];
         };
       };
-    };
+
+      overlays = {
+        # Overlays to add different versions `nixpkgs` into package set
+        pkgs-master = final: prev: {
+          pkgs-master = import inputs.nixpkgs-master {
+            inherit (prev.stdenv) system;
+            inherit (nixpkgsConfig) config;
+          };
+        };
+        pkgs-stable = final: prev: {
+          pkgs-stable = import inputs.nixpkgs-stable {
+            inherit (prev.stdenv) system;
+            inherit (nixpkgsConfig) config;
+          };
+        };
+        pkgs-unstable = final: prev: {
+          pkgs-unstable = import inputs.nixpkgs-unstable {
+            inherit (prev.stdenv) system;
+            inherit (nixpkgsConfig) config;
+          };
+        };
+        apple-silicon = final: prev: optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
+          # Add access to x86 packages system is running Apple Silicon
+          pkgs-x86 = import inputs.nixpkgs-unstable {
+            system = "x86_64-darwin";
+            inherit (nixpkgsConfig) config;
+          };
+        };
+      };
+    } //
+    flake-utils.lib.eachDefaultSystem (system: {
+      legacyPackages = import inputs.nixpkgs-unstable {
+        inherit system;
+        inherit (nixpkgsConfig) config;
+        overlays = with self.overlays; [
+          pkgs-master
+          pkgs-stable
+          apple-silicon
+        ];
+      };
+    });
 }
