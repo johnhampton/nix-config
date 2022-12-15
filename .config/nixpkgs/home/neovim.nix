55867a60dd063166999a7013b32c91c9ee34e577
John Hampton <john.hampton@stanfordalumni.org>
2022-12-15 10:06:00 -0800

    Configure neovim colorscheme

--- .config/nixpkgs/home/neovim.nix
+++ .config/nixpkgs/home/neovim.nix
@@ -1,5 +1,10 @@
 { pkgs, ... }:
 
+let
+
+
+in
+
 { 
   programs.neovim = {
     enable = true;
@@ -8,6 +13,8 @@
   };
 
   home.file.".config/nvim/lua/johnhampton/init.lua".source = ./neovim/lua/johnhampton/init.lua;
+
+  home.file.".config/nvim/lua/johnhampton/colorscheme.lua".source = ./neovim/lua/johnhampton/colorscheme.lua;
   home.file.".config/nvim/lua/johnhampton/keymaps.lua".source = ./neovim/lua/johnhampton/keymaps.lua;
   home.file.".config/nvim/lua/johnhampton/options.lua".source = ./neovim/lua/johnhampton/options.lua;
   home.file.".config/nvim/lua/johnhampton/settings.lua".source = ./neovim/lua/johnhampton/settings.lua;
