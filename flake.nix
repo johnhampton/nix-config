644e8d29a6d52e894ebfc62c98d57172f739327a
John Hampton <john.hampton@stanfordalumni.org>
2023-03-04 14:24:02 -0800

    Remove unused overlay cruft

--- flake.nix
+++ flake.nix
@@ -30,7 +30,6 @@
 
       nixpkgsConfig = {
         config = { allowUnfree = true; };
-        overlays = attrValues self.overlays;
       };
 
 
@@ -76,44 +75,5 @@
         };
       };
 
-      overlays = {
-        # Overlays to add different versions `nixpkgs` into package set
-        pkgs-master = final: prev: {
-          pkgs-master = import inputs.nixpkgs-master {
-            inherit (prev.stdenv) system;
-            inherit (nixpkgsConfig) config;
-          };
-        };
-        pkgs-stable = final: prev: {
-          pkgs-stable = import inputs.nixpkgs-stable {
-            inherit (prev.stdenv) system;
-            inherit (nixpkgsConfig) config;
-          };
-        };
-        pkgs-unstable = final: prev: {
-          pkgs-unstable = import inputs.nixpkgs-unstable {
-            inherit (prev.stdenv) system;
-            inherit (nixpkgsConfig) config;
-          };
-        };
-        apple-silicon = final: prev: optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
-          # Add access to x86 packages system is running Apple Silicon
-          pkgs-x86 = import inputs.nixpkgs-unstable {
-            system = "x86_64-darwin";
-            inherit (nixpkgsConfig) config;
-          };
-        };
-      };
-    } //
-    flake-utils.lib.eachDefaultSystem (system: {
-      legacyPackages = import inputs.nixpkgs-unstable {
-        inherit system;
-        inherit (nixpkgsConfig) config;
-        overlays = with self.overlays; [
-          pkgs-master
-          pkgs-stable
-          apple-silicon
-        ];
-      };
-    });
+    };
 }
