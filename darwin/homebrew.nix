8381d0790894a6e1c4b3503526c2ead8c9e16200
John Hampton <john.hampton@stanfordalumni.org>
2023-03-05 16:28:50 -0800

    Install some apps from homebrew

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -2,6 +2,23 @@
 {
   homebrew.enable = true;
   homebrew = {
-    casks = [ "logseq" ];
+    onActivation.cleanup = "uninstall";
+
+    casks = [
+      "1password"
+      "1password-cli"
+      "dropbox"
+      "logseq"
+      "notion"
+      "raindropio"
+      "raycast"
+      "slack"
+    ];
+    masApps = {
+      "1Password for Safari" = 1569813296;
+      "Notion Web Clipper" = 1559269364;
+      "Save to Raindrop.io" = 1549370672;
+      Xcode = 497799835;
+    };
   };
 }
