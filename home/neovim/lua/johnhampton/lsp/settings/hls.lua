02e184577e786b8bb3b15427d20ff65a0a21945b
John Hampton <john.hampton@stanfordalumni.org>
2023-03-07 09:11:05 -0800

    Adjust hls settings

--- home/neovim/lua/johnhampton/lsp/settings/hls.lua
+++ home/neovim/lua/johnhampton/lsp/settings/hls.lua
@@ -1,19 +1,20 @@
 return {
-	filetypes = { "haskell", "lhaskell", "cabal" },
-	settings = {
-		haskell = {
-			formattingProvider = "fourmolu",
-			plugin = {
-				refineImports = {
-					codeActionsOn = true,
-					codeLensOn = false,
-				},
-				rename = {
-					config = {
-						crossModule = true,
-					},
-				},
-			},
-		},
-	},
+  cmd = { "haskell-language-server", "--lsp" },
+  filetypes = { "haskell", "lhaskell", "cabal" },
+  settings = {
+    haskell = {
+      formattingProvider = "fourmolu",
+      plugin = {
+        refineImports = {
+          codeActionsOn = true,
+          codeLensOn = false,
+        },
+        rename = {
+          config = {
+            crossModule = true,
+          },
+        },
+      },
+    },
+  },
 }
