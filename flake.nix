8c5233a1cc7903224e3e21eba80dfc383b638c7a
John Hampton <john.hampton@stanfordalumni.org>
2023-03-27 13:52:31 -0700

    Install a newer version of nvim-tree-lua

--- flake.nix
+++ flake.nix
@@ -63,9 +63,8 @@
             inherit (prev.pkgs-master) google-cloud-sdk;
           })
 
-
           (final: prev: {
-            vimPlugins = prev.vimPlugins.extend (super: self: {
+            vimPlugins = prev.vimPlugins.extend (vfinal: vprev: {
               "telescope-hoogle-nvim" = final.vimUtils.buildVimPluginFrom2Nix {
                 pname = "telescope-hoogle.nvim";
                 version = inputs.telescope-hoogle-nvim.lastModifiedDate;
@@ -77,6 +76,15 @@
                 version = inputs.focus-nvim.lastModifiedDate;
                 src = inputs.focus-nvim;
               };
+              # We temporarily need this. Remove at a later date.
+              "nvim-tree-lua" = vprev.nvim-tree-lua.overrideAttrs (finalAttrs: prevAttrs: {
+                src = final.fetchFromGitHub {
+                  owner = "nvim-tree";
+                  repo = "nvim-tree.lua";
+                  rev = "a38f9a55a4b55b0aa18af7abfde2c17a30959bdf";
+                  sha256 = "sha256-YC+/+u1UD4eOf0SXTRTmAQe3M9hjAVbTUv0gqkrXGSc=";
+                };
+              });
             });
           })
         ];
