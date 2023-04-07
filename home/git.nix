47684e8e43b85fd81d95e7be207d75bee55cbaac
John Hampton <john.hampton@stanfordalumni.org>
2023-04-07 08:21:33 -0700

    Add global ignore file for git
    Ignore MacOS system files.

--- home/git.nix
+++ home/git.nix
@@ -11,6 +11,34 @@
     delta.options = {
       features = "chameleon";
     };
+    ignores = [
+      # General
+      ".DS_Store"
+      ".AppleDouble"
+      ".LSOverride"
+
+      # Icon must end with two \r
+      "Icon"
+
+      # Thumbnails
+      "._*"
+
+      # Files that might appear in the root of a volume
+      ".DocumentRevisions-V100"
+      ".fseventsd"
+      ".Spotlight-V100"
+      ".TemporaryItems"
+      ".Trashes"
+      ".VolumeIcon.icns"
+      ".com.apple.timemachine.donotpresent"
+
+      # Directories potentially created on remote AFP share
+      ".AppleDB"
+      ".AppleDesktop"
+      "Network Trash Folder"
+      "Temporary Items"
+      ".apdisk"
+    ];
     extraConfig = {
       credential = { helper = "osxkeychain"; };
       pull = { ff = "only"; };
