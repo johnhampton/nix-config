6e23573c611cfc10e997087424740edb481dd435
John Hampton <john.hampton@stanfordalumni.org>
2023-04-08 18:23:06 -0700

    Remove overlay for nvim-tree

--- overlays/vimPlugins.nix
+++ overlays/vimPlugins.nix
@@ -11,16 +11,6 @@
       version = inputs.focus-nvim.lastModifiedDate;
       src = inputs.focus-nvim;
     };
-
-    # We temporarily need this. Remove at a later date.
-    "nvim-tree-lua" = vprev.nvim-tree-lua.overrideAttrs (finalAttrs: prevAttrs: {
-      src = final.fetchFromGitHub {
-        owner = "nvim-tree";
-        repo = "nvim-tree.lua";
-        rev = "a38f9a55a4b55b0aa18af7abfde2c17a30959bdf";
-        sha256 = "sha256-YC+/+u1UD4eOf0SXTRTmAQe3M9hjAVbTUv0gqkrXGSc=";
-      };
-    });
   });
 })
 
