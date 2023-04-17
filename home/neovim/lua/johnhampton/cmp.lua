6d87c50358db83d0709f5c6fce10c2e66e16f75d
John Hampton <john.hampton@stanfordalumni.org>
2023-04-16 18:21:57 -0700

    Fix issue with unpredicatable cursor behaviour in LuaSnip
    Implemented solution detail in
    https://github.com/L3MON4D3/LuaSnip/issues/258

--- home/neovim/lua/johnhampton/cmp.lua
+++ home/neovim/lua/johnhampton/cmp.lua
@@ -122,3 +122,17 @@ cmp.setup({
     native_menu = false,
   },
 })
+
+-- Fix issue with unpredictable LuaSnip cursor behavior
+-- https://github.com/L3MON4D3/LuaSnip/issues/258
+vim.api.nvim_create_autocmd('ModeChanged', {
+  pattern = '*',
+  callback = function()
+    if ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
+        and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
+        and not require('luasnip').session.jump_active
+    then
+      require('luasnip').unlink_current()
+    end
+  end
+})
