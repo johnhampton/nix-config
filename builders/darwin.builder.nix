391c9bc8967f9a52c7b3470d74c660dd4d378585
John Hampton <john.hampton@stanfordalumni.org>
2023-06-02 09:07:38 -0700

    Minor updates to build machines

--- builders/darwin.builder.nix
+++ builders/darwin.builder.nix
@@ -5,8 +5,8 @@
         hostName = "ssh-ng://builder@localhost";
         system = "aarch64-linux";
         sshKey = "/etc/nix/builder_ed25519";
-        supportedFeatures = [ "kvm" ];
-        maxJobs = 8;
+        supportedFeatures = [ "kvm" "benchmark" "big-parallel" ];
+        maxJobs = 4;
         publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUpCV2N4Yi9CbGFxdDFhdU90RStGOFFVV3JVb3RpQzVxQkorVXVFV2RWQ2Igcm9vdEBuaXhvcwo=";
       }
     ];
--- builders/nixbuild.nix
+++ builders/nixbuild.nix
@@ -10,14 +10,14 @@
     #   publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSVBJUUNaYzU0cG9KOHZxYXdkOFRyYU5yeVFlSm52SDFlTHBJRGdiaXF5bU0K";
     # }
     buildMachines = [
-      {
-        hostName = "eu.nixbuild.net";
-        system = "aarch64-linux";
-        maxJobs = 100;
-        supportedFeatures = [ "benchmark" "big-parallel" ];
-        sshKey = config.age.secrets.ssh_key.path;
-        publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSVBJUUNaYzU0cG9KOHZxYXdkOFRyYU5yeVFlSm52SDFlTHBJRGdiaXF5bU0K";
-      }
+      # {
+      #   hostName = "eu.nixbuild.net";
+      #   system = "aarch64-linux";
+      #   maxJobs = 100;
+      #   supportedFeatures = [ "benchmark" "big-parallel" ];
+      #   sshKey = config.age.secrets.ssh_key.path;
+      #   publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSVBJUUNaYzU0cG9KOHZxYXdkOFRyYU5yeVFlSm52SDFlTHBJRGdiaXF5bU0K";
+      # }
 
     ];
   };
