7f9a60bccabd6243e8b34bd9e92f028e8d673fd8
John Hampton <john.hampton@stanfordalumni.org>
2023-08-21 15:55:21 -0700

    Update focus config

--- home/neovim/lua/johnhampton/focus.lua
+++ home/neovim/lua/johnhampton/focus.lua
@@ -14,23 +14,28 @@ local augroup =
     vim.api.nvim_create_augroup('FocusDisable', { clear = true })
 
 vim.api.nvim_create_autocmd('WinEnter', {
-    group = augroup,
-    callback = function(_)
-        if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
-            vim.b.focus_disable = true
-        end
-    end,
-    desc = 'Disable focus autoresize for BufType',
+  group = augroup,
+  callback = function(_)
+    if vim.tbl_contains(ignore_buftypes, vim.bo.buftype)
+    then
+      vim.w.focus_disable = true
+    else
+      vim.w.focus_disable = false
+    end
+  end,
+  desc = 'Disable focus autoresize for BufType',
 })
 
-vim.api.nvim_create_autocmd('FileType', {
-    group = augroup,
-    callback = function(_)
-        if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
-            vim.b.focus_disable = true
-        end
-    end,
-    desc = 'Disable focus autoresize for FileType',
+vim.api.nvim_create_autocmd({ 'BufWinEnter', 'FileType' }, {
+  group = augroup,
+  callback = function(_)
+    if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+      vim.w.focus_disable = true
+    else
+      vim.w.focus_disable = false
+    end
+  end,
+  desc = 'Disable focus autoresize for FileType',
 })
 
 vim.keymap.set('n', '<leader>wn', '<cmd>FocusSplitNicely<cr>', { desc = "New Split" })
