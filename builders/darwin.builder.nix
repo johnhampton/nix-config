b5df593908b0b8c3f323f7e46ef36f01c42992a5
John Hampton <john.hampton@stanfordalumni.org>
2023-09-28 16:19:16 -0700

    Configure darwin builder

--- builders/darwin.builder.nix
+++ builders/darwin.builder.nix
@@ -1,15 +1,14 @@
 { ... }: {
   nix = {
-    buildMachines = [
-      {
-        hostName = "ssh-ng://builder@localhost";
-        system = "aarch64-linux";
-        sshKey = "/etc/nix/builder_ed25519";
-        supportedFeatures = [ "kvm" "benchmark" "big-parallel" ];
-        maxJobs = 4;
-        publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUpCV2N4Yi9CbGFxdDFhdU90RStGOFFVV3JVb3RpQzVxQkorVXVFV2RWQ2Igcm9vdEBuaXhvcwo=";
-      }
-    ];
+    linux-builder.enable = true;
+    linux-builder = {
+      maxJobs = 4;
+      modules = [
+        ({ ... }: {
+          virtualisation.darwin-builder.memorySize = 8192;
+        })
+      ];
+    };
   };
 }
      
