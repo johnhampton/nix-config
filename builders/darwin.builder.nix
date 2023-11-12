3e9dcd526d68d348ff808555d182ebbba1389ae7
John Hampton <john.hampton@stanfordalumni.org>
2023-11-12 11:07:57 -0800

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'darwin':
        'github:LnL7/nix-darwin/afe83cbc2e673b1f08d32dd0f70df599678ff1e7' (2023-10-27)
      → 'github:LnL7/nix-darwin/0f1ad801387445fdda01d080db8ecf169be8e793' (2023-11-11)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/8765d4e38aa0be53cdeee26f7386173e6c65618d' (2023-11-03)
      → 'github:nix-community/home-manager/280721186ab75a76537713ec310306f0eba3e407' (2023-11-12)
    • Updated input 'nix-index-database':
        'github:Mic92/nix-index-database/1f0981f5baeb78e3c89a8980ff1a39f06876fa8c' (2023-10-29)
      → 'github:Mic92/nix-index-database/8aff4ca3dee60d1422489fe8d52c2f837b3ad113' (2023-11-12)
    • Updated input 'nixpkgs-master':
        'github:nixos/nixpkgs/4eafd278855a6e5fabe9a4a35bda74beab60534d' (2023-11-04)
      → 'github:nixos/nixpkgs/e0ad52a0a8eb1395246a99956ffe264a0ba57160' (2023-11-12)
    • Updated input 'nixpkgs-unstable':
        'github:NixOS/nixpkgs/9d5d25bbfe8c0297ebe85324addcb5020ed1a454' (2023-11-04)
      → 'github:NixOS/nixpkgs/911ad1e67f458b6bcf0278fa85e33bb9924fed7e' (2023-11-11)

--- builders/darwin.builder.nix
+++ builders/darwin.builder.nix
@@ -3,13 +3,10 @@
     linux-builder.enable = true;
     linux-builder = {
       maxJobs = 4;
-      modules = [
-        ({ ... }: {
-          virtualisation.darwin-builder.memorySize = 8192;
-          virtualisation.darwin-builder.diskSize = 10240;
-        })
-      ];
+      config = {
+        virtualisation.darwin-builder.memorySize = 8192;
+        virtualisation.darwin-builder.diskSize = 10240;
+      };
     };
   };
 }
-     
