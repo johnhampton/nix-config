4752d888bd8852c958130218970fd21278688656
John Hampton <john.hampton@stanfordalumni.org>
2024-06-07 11:20:09 -0700

    feat(mini.nix): Update key bindings and actions for MiniFiles
    This commit updates the key bindings and actions for MiniFiles in
    `mini.nix`. The `<leader>e` key now toggles the MiniFiles explorer,
    while the `-` key opens the explorer for the directory of the current
    file. The `<leader>E` key now opens a fresh explorer. These changes
    improve the usability and flexibility of the MiniFiles explorer.

--- home/nixvim/mini.nix
+++ home/nixvim/mini.nix
@@ -15,17 +15,31 @@
       {
         mode = "n";
         key = "<leader>e";
-        action = "<cmd>lua MiniFiles.open()<cr>";
+        action = {
+          __raw = ''
+            function () 
+              if not MiniFiles.close() then MiniFiles.open() end
+            end
+          '';
+        };
         options = {
           desc = "Explore";
         };
       }
       {
         mode = "n";
-        key = "<leader>E";
+        key = "-";
         action = "<cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<cr>";
         options = {
-          desc = "Explore current file";
+          desc = "Explore directory of current file";
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>E";
+        action = "<cmd>lua MiniFiles.open(nil, false)<cr>";
+        options = {
+          desc = "Fresh explorer";
         };
       }
     ];
