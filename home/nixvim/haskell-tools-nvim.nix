f4dde598ab7cb91b079cae6381324a256b4759ac
John Hampton <john.hampton@stanfordalumni.org>
2024-06-06 17:48:42 -0700

    feat(haskell-tools-nvim): enhance Haskell support in Neovim configuration
    -  Added `telescope_hoogle` to `extraPlugins`.
    -  Configured `which-key` registrations for Haskell-related commands.
    -  Added keymaps for Hoogle search, signature search, evaluating code snippets, and managing GHCi REPL sessions.
      - `<leader>hh`: Hoogle search
      - `<leader>hs`: Hoogle signature search
      - `<leader>he`: Evaluate all code snippets
      - `<leader>hrr`: Toggle a GHCi REPL for the current package
      - `<leader>hrf`: Toggle a GHCi REPL for the current buffer
      - `<leader>hrq`: Quit the GHCi REPL

--- home/nixvim/haskell-tools-nvim.nix
+++ home/nixvim/haskell-tools-nvim.nix
@@ -1,14 +1,92 @@
 { pkgs, ... }:
 {
   programs.nixvim = {
-    extraPlugins = [ pkgs.vimPlugins.haskell-tools-nvim ];
-		extraPackages = [
-		  pkgs.haskellPackages.fast-tags
-			pkgs.haskellPackages.haskell-debug-adapter
-		];
-
+    extraPlugins = [
+      pkgs.vimPlugins.haskell-tools-nvim
+      pkgs.vimPlugins.telescope_hoogle
+    ];
+    extraPackages = [
+      pkgs.haskellPackages.fast-tags
+      pkgs.haskellPackages.haskell-debug-adapter
+    ];
+    plugins = {
+      telescope.enabledExtensions = [
+        "hoogle"
+      ];
+      which-key.registrations = {
+        "<leader>h" = { name = "Haskell"; };
+        "<leader>hr" = { name = "REPL"; };
+      };
+    };
     files = {
       "after/ftplugin/haskell.lua" = {
+        keymaps = [
+          {
+            key = "<leader>hh";
+            action = "<cmd>Telescope hoogle<cr>";
+            mode = "n";
+            options = {
+              buffer = true;
+              desc = "Hoogle search";
+            };
+          }
+          {
+            key = "<leader>hs";
+            action = { __raw = ''require("haskell-tools").hoogle.hoogle_signature''; };
+            mode = "n";
+            options = {
+              noremap = true;
+              silent = true;
+              buffer = true;
+              desc = "Hoogle signature search";
+            };
+          }
+          {
+            key = "<leader>he";
+            action = { __raw = ''require("haskell-tools").lsp.buf_eval_all''; };
+            mode = "n";
+            options = {
+              noremap = true;
+              silent = true;
+              buffer = true;
+              desc = "Evaluate all code snippets";
+            };
+          }
+          {
+            key = "<leader>hrr";
+            action = { __raw = ''require("haskell-tools").repl.toggle''; };
+            mode = "n";
+            options = {
+              noremap = true;
+              silent = true;
+              buffer = true;
+              desc = "Toggle a GHCi repl for the current package";
+            };
+          }
+          {
+            key = "<leader>hrf";
+            action = { __raw = ''function() require("haskell-tools").repl.toggle(vim.api.nvim_buf_get_name(0)) end''; };
+            mode = "n";
+            options = {
+              noremap = true;
+              silent = true;
+              buffer = true;
+              desc = "Toggle a GHCi repl for the current buffer";
+            };
+          }
+          {
+            key = "<leader>hrq";
+            action = { __raw = ''require("haskell-tools").repl.quit''; };
+            mode = "n";
+            options = {
+              noremap = true;
+              silent = true;
+              buffer = true;
+              desc = "Quit the GHCi repl";
+            };
+          }
+        ];
+
         extraConfigLua = ''
           require('haskell-tools')
         '';
