7f4ddba0f5039e133a35751dfdb20e0cfc9b34ee
John Hampton <john.hampton@stanfordalumni.org>
2024-07-03 14:04:36 -0400

    feat(mini.nix): add split window functionality to MiniFiles
    This commit introduces a new feature in the MiniFiles plugin. It allows
    users to split the window either horizontally or vertically. The keys
    'gs' and 'gv' have been mapped to perform the belowright horizontal and
    belowright vertical splits respectively. This feature enhances the
    usability of the MiniFiles plugin by providing more flexibility in
    window management.

--- home/nixvim/mini.nix
+++ home/nixvim/mini.nix
@@ -21,6 +21,8 @@
       };
     };
 
+
+
     autoCmd = [
       {
         event = "User";
@@ -31,6 +33,38 @@
               local buf_id = args.data.buf_id
 
               vim.keymap.set('n', '-', MiniFiles.go_out, { buffer = buf_id, desc = "Go out" })
+            end
+          '';
+        };
+      }
+      {
+        event = "User";
+        pattern = "MiniFilesBufferCreate";
+        callback = {
+          __raw = ''
+            function(args)
+              local map_split = function(buf_id, lhs, direction)
+                local rhs = function()
+                  -- Make new window and set it as target
+                  local new_target_window
+                  vim.api.nvim_win_call(MiniFiles.get_target_window(), function()
+                    vim.cmd(direction .. ' split')
+                    new_target_window = vim.api.nvim_get_current_win()
+                  end)
+
+                  MiniFiles.set_target_window(new_target_window)
+                end
+
+                -- Adding `desc` will result into `show_help` entries
+                local desc = 'Split ' .. direction
+                vim.keymap.set('n', lhs, rhs, { buffer = buf_id, desc = desc })
+              end
+
+              local buf_id = args.data.buf_id
+
+              -- Tweak keys to your liking
+              map_split(buf_id, 'gs', 'belowright horizontal')
+              map_split(buf_id, 'gv', 'belowright vertical')
             end
           '';
         };
@@ -106,7 +140,7 @@
         };
       }
       {
-        mode = ["n" "x"];
+        mode = [ "n" "x" ];
         key = "<leader>gs";
         action = "<cmd>lua MiniGit.show_at_cursor()<cr>";
         options = {
