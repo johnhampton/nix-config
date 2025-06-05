9f21fc9751541e611ccb91a5a5cd9eb434fe0f8c
John Hampton <john.hampton@stanfordalumni.org>
2025-06-05 15:49:51 -0700

    fix(tmux): update terminal RGB capability setting
    Replace deprecated "terminal-overrides" option with the newer
    "terminal-features" option for RGB color support in tmux.

--- home/tmux/default.nix
+++ home/tmux/default.nix
@@ -25,7 +25,7 @@
 
     extraConfig = ''
       # https://gist.github.com/andersevenrud/015e61af2fd264371032763d4ed965b6
-      set -sg terminal-overrides ",*:RGB"
+      set -as terminal-features ",*:RGB"
 
       set -g renumber-windows on
 
