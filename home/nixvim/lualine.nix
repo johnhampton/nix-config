8663594fb33dfa0c37bd8384c8ceaea3ff7374b3
John Hampton <john.hampton@stanfordalumni.org>
2025-06-07 09:19:20 -0700

    feat(lualine): add mini.sessions status indicator
    Add session status to lualine with:
    - Display current session name with file extension
    - Different icons for local vs global sessions (folder/globe)
    - OneNord blue color (#81A1C1) with alternate color options commented
    - Position in lualine_x section for better visual balance

--- home/nixvim/lualine.nix
+++ home/nixvim/lualine.nix
@@ -10,6 +10,32 @@
             "filename"
             "lsp_progress"
           ];
+          lualine_x = [
+            {
+              __raw = ''
+                {
+                  function()
+                    if vim.v.this_session and vim.v.this_session ~= "" then
+                      local session_name = vim.fn.fnamemodify(vim.v.this_session, ":t")
+                      local is_local = session_name == "Session.vim"
+                      local icon = is_local and "" or ""
+                      return icon .. " " .. session_name
+                    end
+                    return ""
+                  end,
+                  -- OneNord blue
+                  color = { fg = "#81A1C1" },
+                  -- Alternative OneNord colors:
+                  -- color = { fg = "#88C0D0" }, -- cyan
+                  -- color = { fg = "#B988B0" }, -- purple  
+                  -- color = { fg = "#4C566A" }, -- subtle gray
+                }
+              '';
+            }
+            "encoding"
+            "fileformat"
+            "filetype"
+          ];
         };
       };
     };
