3c440f158ed764d784ff62d40dc7942ea509da21
John Hampton <john.hampton@stanfordalumni.org>
2025-06-07 18:13:14 -0700

    feat(darwin): disable Spotlight keyboard shortcuts
    - Disable key 64 (Show Spotlight search) - Cmd+Space
    - Disable key 65 (Show Finder search window) - Cmd+Option+Space
    - Add activateSettings command to ensure changes take effect immediately

--- darwin/hotkeys.nix
+++ darwin/hotkeys.nix
@@ -7,15 +7,23 @@
         "120" = 20; # ^3 to switch to Desktop 3
         "121" = 21; # ^4 to switch to Desktop 4
         "122" = 23; # ^5 to switch to Desktop 5
-        "123" = 22;
-        "124" = 26;
-        "125" = 28;
-        "126" = 25;
+        "123" = 22; # ^6 to switch to Desktop 6
+        "124" = 26; # ^7 to switch to Desktop 7
+        "125" = 28; # ^8 to switch to Desktop 8
+        "126" = 25; # ^9 to switch to Desktop 9
+        "27" = 8; # Hyper+C to move focus to next window
       };
 
       enableHotKeysCommands = builtins.map
         (key:
-          let value = keys.${key}; in ''
+          let
+            value = keys.${key};
+            # Use Hyper modifier for key 27, Control modifier for others
+            modifier = if key == "27" then "1966080" else "262144";
+            # Use ASCII code 99 for 'c' on key 27, 65535 for Control+number shortcuts
+            asciiCode = if key == "27" then "99" else "65535";
+          in
+          ''
                 sudo -u john defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add ${key} '
             		<dict>
             			<key>enabled</key>
@@ -24,9 +32,9 @@
             			<dict>
             				<key>parameters</key>
             				<array>
-            					<integer>65535</integer>
+            					<integer>${asciiCode}</integer>
             					<integer>${toString value}</integer>
-            					<integer>262144</integer>
+            					<integer>${modifier}</integer>
             				</array>
             				<key>type</key>
             				<string>standard</string>
@@ -35,11 +43,29 @@
           '')
         (builtins.attrNames keys);
 
+      # Disable Spotlight shortcuts
+      disableSpotlightCommands = [
+        # Key 64: Show Spotlight search (Cmd+Space)
+        ''
+          sudo -u john defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 64 '<dict><key>enabled</key><false/></dict>'
+        ''
+        # Key 65: Show Finder search window (Cmd+Option+Space)
+        ''
+          sudo -u john defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 65 '<dict><key>enabled</key><false/></dict>'
+        ''
+      ];
+
     in
     ''
        echo >&2 "configuring hotkeys..."
 
       ${builtins.concatStringsSep "\n" enableHotKeysCommands}
+      
+      echo >&2 "disabling spotlight shortcuts..."
+      ${builtins.concatStringsSep "\n" disableSpotlightCommands}
+      
+      echo >&2 "activating settings..."
+      sudo -u john /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
     '';
 
 }
