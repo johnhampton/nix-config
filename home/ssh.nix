f2ad67e2d330bcfb0ab45e1fa6e0dd34eef93197
John Hampton <john.hampton@stanfordalumni.org>
2025-09-03 14:39:28 -0700

    fix(ssh): preserve all original SSH config settings
    - Set enableDefaultConfig to false to prevent future defaults
    - Include all original Host * settings:
      - ForwardAgent, Compression, ServerAlive settings
      - HashKnownHosts, UserKnownHostsFile
      - ControlMaster, ControlPath, ControlPersist
      - AddKeysToAgent and UseKeychain for macOS
    - Maintain include for colima SSH config

--- home/ssh.nix
+++ home/ssh.nix
@@ -4,18 +4,24 @@
   programs.ssh = {
     enableDefaultConfig = false;
     
-    extraConfig = ''
-      AddKeysToAgent yes
-      UseKeychain yes
-    '';
-
     includes = [
       "${config.home.homeDirectory}/.colima/ssh_config"
     ];
     
     matchBlocks."*" = {
+      addKeysToAgent = "yes";
+      forwardAgent = false;
+      compression = false;
+      serverAliveInterval = 0;
+      serverAliveCountMax = 3;
+      hashKnownHosts = false;
+      userKnownHostsFile = "~/.ssh/known_hosts";
+      controlMaster = "no";
+      controlPath = "~/.ssh/master-%r@%n:%p";
+      controlPersist = "no";
+
       extraOptions = {
-        # Add any default options you want to keep from the default config here
+        UseKeychain = "yes";
       };
     };
   };
