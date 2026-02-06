a66371ad01fcd5b1e785c647dc20138c24623411
John Hampton <john.hampton@stanfordalumni.org>
2026-02-06 15:38:39 -0800

    feat: set wezterm font size to 14pt for 6K display
    Configure font size for Apple Pro Display XDR (32" 6K).
    Remove unused window_frame font override.
    
    - Set font_size to 14.0 for optimal density at 218 PPI
    - Remove window_frame font configuration
    - Keep PragmataPro Mono Liga as primary font

--- home/wezterm/wezterm.lua
+++ home/wezterm/wezterm.lua
@@ -9,11 +9,7 @@ end
 
 config.use_cap_height_to_scale_fallback_fonts = true;
 config.font = wezterm.font 'PragmataPro Mono Liga'
-config.font_size = 15.0
-config.window_frame = {
-  font = wezterm.font { family = 'PragmataPro Mono Liga', weight = 'Bold' },
-  font_size = 14.5,
-}
+config.font_size = 14.0
 
 config.color_scheme = 'OneNord'
 -- config.use_fancy_tab_bar = false
